76f2b791493d69021aa9fd026a386bee8560142d
Michael Brooks <mikeyabrooks@gmail.com>
2017-10-13 10:13:55 -0700

    add first passing test of year to seconds

---
+++ js/agecalc-interface.js
@@ -0,0 +1,18 @@
+import { AgeCalc } from './../js/agecalc.js';
+
+$(document).ready(function() {
+  $('#haiku-form').submit(function(event) {
+    event.preventDefault();
+    let line1 = $('#line1').val();
+    let line2 = $('#line2').val();
+    let line3 = $('#line3').val();
+    var haiku = new Poem(line1,line2,line3);
+    var solution = haiku.isHaiku();
+    if (solution === true) {
+      $('#solution').text("You have written a beautiful haiku");
+    }
+    else {
+      $('#solution').text("You have failed at haiku'ing, please try again!");
+    }
+  });
+});
---
+++ spec/agecalc-spec.js
@@ -0,0 +1,22 @@
+import { AgeCalc } from './../js/agecalc.js';
+
+describe('AgeCalc', function() {
+  it('will convert years into seconds', function() {
+    let input = new AgeCalc(1);
+    expect(input.yearsToSeconds()).toEqual(31536000);
+  });
+});
+  // it('will scan an input and return the vowels count', function() {
+  //   let vowel = new Poem("The red foxes", "Jump over the fence.", "Likes to eat apples!");
+  //   expect(vowel.vowelCount()).toEqual([4, 6, 6])
+  // });
+  //
+  // it('will remove slient e\'s at the end of any words in our string input', function() {
+  //   let input = new Poem("The red foxes", "Jump over the fence.", "Likes to eat apples!");
+  //   expect(input.eRemoval()).toEqual('The red foxesJump over the fencLikes to eat apples')
+  // });
+  //
+  // it('will tell us if the input is a haiku', function() {
+  //   let solution = new Poem("The redish foxes", "Jump high over the brown fence.", "Likes to eat apples!");
+  //   expect(solution.isHaiku()).toEqual(true)
+  // });
