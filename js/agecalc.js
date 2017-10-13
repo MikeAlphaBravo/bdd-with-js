bf5c8da5c0653b388fac238e73439219bd8a31c0
Michael Brooks <mikeyabrooks@gmail.com>
2017-10-13 15:35:17 -0700

    add commented out future functionality but not quite passing specs on main function.  Good point to snapshot where I am at

--- js/agecalc.js
+++ js/agecalc.js
@@ -1,3 +1,5 @@
+import { moment } from './../node_modules/moment/moment.js';
+
 export class AgeCalc {
   constructor(age, inputDate) {
   this.age = age;
@@ -9,4 +11,42 @@ export class AgeCalc {
     let ageSeconds = age * yearSeconds;
     return ageSeconds;
   }
+
+  userAge(date) {
+    let presentDateSeconds = new Date() / 1000;
+    let inputDateSeconds = new Date(date) / 1000;
+    let ageInSeconds = presentDateSeconds - inputDateSeconds;
+    let ageInYears = ageInSeconds / 31536000;
+    console.log(ageInYears);
+  }
+
+  // ageEarth(age) {
+  //   const earth = 1;
+  //   let ageOnEarth = Math.round((age * earth) *100)/100;
+  //   return ageOnEarth
+  // }
+  //
+  // ageMercury(age) {
+  //   const mercury = 0.24;
+  //   let ageOnMercury = Math.round((age * mercury) *100)/100;
+  //   return ageOnMercury
+  // }
+  //
+  // ageVenus(age) {
+  //   const venus = 0.62;
+  //   let ageOnVenus = Math.round((age * venus) *100)/100;
+  //   return ageOnVenus
+  // }
+  //
+  // ageMars(age) {
+  //   const mars = 1.88;
+  //   let ageOnMars = Math.round((age * mars) *100)/100;
+  //   return ageOnMars
+  // }
+  //
+  // ageJupiter(age) {
+  //   const jupiter = 11.86;
+  //   let ageOnJupiter = Math.round((age * jupiter) *100)/100;
+  //   return ageOnJupiter
+  // }
 }
--- spec/agecalc-spec.js
+++ spec/agecalc-spec.js
@@ -1,16 +1,16 @@
 import { AgeCalc } from './../js/agecalc.js';
 
 describe('AgeCalc', function() {
-  // let now = require('now');
+  // let now = require('moment');
   let date;
   // let testingDate;
   // let dateTesting;
   // let currentTestingDate;
-  // let inputDate;
+  let inputDate;
   beforeEach(function(){
   //   testingDate = "1982-27-07";
     date = new AgeCalc();
-  //   inputDate = date.currentAge("1982/07/27");
+    inputDate = "1982-07-27";
   //   dateTesting = now(testingDate);
   //   currentTestingDate = now();
   });
@@ -22,13 +22,29 @@ describe('AgeCalc', function() {
   it('will convert years into seconds', function() {
     expect(date.yearsToSeconds(5)).toEqual(157680000);
   });
-});
 
-//
-// it('will convert years into seconds', function() {
-//   expect(date.yearsToSeconds(1)).toEqual(31536000);
-// });
-//
-// it('will convert years into seconds', function() {
-//   expect(date.yearsToSeconds(5)).toEqual(157680000);
-// });
+  it('will return the age of the user in years', function() {
+    expect(date.userAge("1982-07-27")).toEqual(35.24090091140284);
+  });
+
+  // it('will check for user age on Earth', function() {
+  //   expect(date.ageEarth(inputDate)).toEqual(35)
+  // });
+  //
+  // it('will check for user age on Mercury', function() {
+  //   expect(date.ageEarth(inputDate)).toEqual(8.4)
+  // });
+  //
+  // it('will check for user age on Venus', function() {
+  //   expect(date.ageEarth(inputDate)).toEqual(21.7)
+  // });
+  //
+  // it('will check for user age on Mars', function() {
+  //   expect(date.ageEarth(inputDate)).toEqual(65.8)
+  // });
+  //
+  // it('will check for user age on Jupiter', function() {
+  //   expect(date.ageEarth(inputDate)).toEqual(415.1)
+  // });
+
+});
