c1a6189c1cc02126950897e558819ad49dbea137
Michael Brooks <mikeyabrooks@gmail.com>
2017-10-13 12:09:43 -0700

    add refactored seconds conversion to allow for expansion with check between times

--- js/agecalc.js
+++ js/agecalc.js
@@ -1,56 +1,12 @@
 export class AgeCalc {
-  constructor(age) {
-  this.age = parseInt(age);
+  constructor(age, inputDate) {
+  this.age = age;
+  this.inputDate = inputDate
 }
 
-  yearsToSeconds() {
+  yearsToSeconds(age) {
     const yearSeconds = 31536000;
-    let ageSeconds = this.age * yearSeconds;
+    let ageSeconds = age * yearSeconds;
     return ageSeconds;
   }
 }
-//   isHaiku(input) {
-//     if (this.syllableChecker(this.line1, 5) &&
-//         this.syllableChecker(this.line2, 7) &&
-//         this.syllableChecker(this.line3, 5)) {
-//       return true;
-//     }
-//   }
-//
-//   vowelCounter(input) {
-//     return (input.match(/[aeiouy]{1,2}/gi,'')).length;
-//   }
-//
-//   eDestroyer(input){
-//     let words = input.replace(/[^\w\s]|_/, "").split(" ");
-//     words = words.map((word) => {
-//       if (word.charAt(word.length - 1) === 'e' && word.length >= 3) {
-//         return word.substr(0, word.length - 1);
-//       } else {
-//         return word;
-//       }
-//     });
-//     return words.join(" ");
-//   }
-//
-//   destroyAndCount(input) {
-//     let result;
-//
-//     result = this.eDestroyer(input);
-//     result = this.vowelCounter(result);
-//
-//     return result;
-//   }
-//
-//   syllableChecker(input,  desiredCount) {
-//     let result;
-//
-//     result = this.destroyAndCount(input);
-//
-//     if (result === desiredCount) {
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   }
-// }
--- spec/agecalc-spec.js
+++ spec/agecalc-spec.js
@@ -1,27 +1,34 @@
 import { AgeCalc } from './../js/agecalc.js';
 
 describe('AgeCalc', function() {
-  it('will convert years into seconds', function() {
-    let input = new AgeCalc(1);
-    expect(input.yearsToSeconds()).toEqual(31536000);
+  // let now = require('now');
+  let date;
+  // let testingDate;
+  // let dateTesting;
+  // let currentTestingDate;
+  // let inputDate;
+  beforeEach(function(){
+  //   testingDate = "1982-27-07";
+    date = new AgeCalc();
+  //   inputDate = date.currentAge("1982/07/27");
+  //   dateTesting = now(testingDate);
+  //   currentTestingDate = now();
   });
 
   it('will convert years into seconds', function() {
-    let input = new AgeCalc(5);
-    expect(input.yearsToSeconds()).toEqual(157680000);
+    expect(date.yearsToSeconds(1)).toEqual(31536000);
+  });
+
+  it('will convert years into seconds', function() {
+    expect(date.yearsToSeconds(5)).toEqual(157680000);
   });
 });
-  // it('will scan an input and return the vowels count', function() {
-  //   let vowel = new Poem("The red foxes", "Jump over the fence.", "Likes to eat apples!");
-  //   expect(vowel.vowelCount()).toEqual([4, 6, 6])
-  // });
-  //
-  // it('will remove slient e\'s at the end of any words in our string input', function() {
-  //   let input = new Poem("The red foxes", "Jump over the fence.", "Likes to eat apples!");
-  //   expect(input.eRemoval()).toEqual('The red foxesJump over the fencLikes to eat apples')
-  // });
-  //
-  // it('will tell us if the input is a haiku', function() {
-  //   let solution = new Poem("The redish foxes", "Jump high over the brown fence.", "Likes to eat apples!");
-  //   expect(solution.isHaiku()).toEqual(true)
-  // });
+
+//
+// it('will convert years into seconds', function() {
+//   expect(date.yearsToSeconds(1)).toEqual(31536000);
+// });
+//
+// it('will convert years into seconds', function() {
+//   expect(date.yearsToSeconds(5)).toEqual(157680000);
+// });
